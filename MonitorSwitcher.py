5818ce848ad8aabd68f1672a6b181558e4729d34
xeroxYor <lbbas@126.com>
2024-11-27 11:07:22 +0800

    add restore func&hotkey

--- MonitorSwitcher.py
+++ MonitorSwitcher.py
@@ -28,6 +28,12 @@ def switch_outer():
     with right:
         right.set_input_source(InputSource.ANALOG1)
 
+def restore():
+    with left:
+        left.set_input_source(InputSource.ANALOG1)
+    with right:
+        right.set_input_source(InputSource.ANALOG1)
+
 def quit():
     icon.stop()
     os._exit(1)
@@ -40,12 +46,14 @@ icon = Icon(
     menu=Menu(
         MenuItem("切换到内网(crtl+f11)", switch_inner),
         MenuItem("切换到外网(crtl+f12)", switch_outer),
+        MenuItem("恢复原状(crtl+insert)", restore),
         MenuItem("退出", quit),
     ),
 )
 # 绑定快捷键
 keyboard.add_hotkey("ctrl+f11", switch_inner)
 keyboard.add_hotkey("ctrl+f12", switch_outer)
+keyboard.add_hotkey("ctrl+insert", restore)
 
 # 运行托盘图标及监听快捷键
 icon.run()
